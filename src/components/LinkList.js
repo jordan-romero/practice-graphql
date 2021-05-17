c9f7c2835c6999ad52d75650df2b04a1c5539fa6
Jordan Romero <jordanromero1390@gmail.com>
2021-05-17 14:45:17 -0500

    this tutorial is meh

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,14 +2,25 @@ import React from 'react';
 import Link from './Link';
 import { useQuery, gql } from '@apollo/client';
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
+      id
       links {
         id
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
@@ -20,8 +31,13 @@ const LinkList = () => {
 
   return (
     <div>
-      {data &&
-        data.feed.links.map((link) => <Link key={link.id} link={link} />)}
+      {data && (
+        <>
+          {data.feed.links.map((link, index) => (
+            <Link key={link.id} link={link} index={index} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
